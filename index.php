b6bb9fd441af1e92bb01f39c522a036091e14fa5
Sampo Turve <sampo.turve@wunder.io>
2023-05-02 10:05:01 +0300

    Copy over the age-old site things from the Linode.

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html dir="ltr" class="js" lang="en"><head>
+  <title>Sampo Turve 🧃🦆</title>
+  <meta charset="utf-8">
+  <meta name="description" content="Web developer who does Drupal and other web things. Likes bikes and birds.">
+  <meta name="rights" content="Sampo Turve">
+  <meta name="MobileOptimized" content="width">
+  <meta name="HandheldFriendly" content="true">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta property="og:site_name" content="Sampo Turve" />
+  <meta property="og:url" content="https://sampoturve.fi/" />
+  <meta property="og:title" content="Sampo Turve" />
+  <meta property="og:description" content="Web developer who does Drupal and other web things. Likes bikes and birds." />
+
+  <link rel="canonical" href="https://sampoturve.fi/">
+  <link rel="shortlink" href="https://sampoturve.fi/">
+
+  <link type="text/css" rel="stylesheet" href="res/css.css" media="all">
+  <link rel="apple-touch-icon" sizes="180x180" href="/res/meta/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/res/meta/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/res/meta/favicon-16x16.png">
+  <link rel="manifest" href="/res/meta/site.webmanifest">
+  <link rel="mask-icon" href="/res/meta/safari-pinned-tab.svg" color="#000000">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="theme-color" content="#ffffff">
+</head>
+<body>
+  <div class="party">
+  <div class="page">
+    <header class="c c--f">
+      <h1>
+        <a href="https://sampoturve.fi/" rel="home">Sampo Turve</a>
+      </h1>
+      <h2>Web developer who does Drupal and other web things. Likes bikes and birds.</h2>
+    </header>
+
+    <main>
+      <article>
+        <div class="c c--h">
+          <img alt="Developer's head on a table" src="res/spaaaa.jpg">
+        </div>
+        <div class="c c--h">
+          <p>I'm Sampo. I’m working as a developer at <a href="https://wunder.io/" target="blank" class="wunder" title="Wunder">Wunder</a>.</p>
+          <p>I’m located in Turku, Finland.</p>
+          <p>I like pretty things and bikes.</p>
+          <div class="contact-links">
+            <a href="https://fi.linkedin.com/in/sampoturve" title="LinkedIn" class="linkedin" target="_blank">LinkedIn</a>
+            <?php // <a href="https://drupal.org/user/669530" title="Drupal.org" class="drupal" target="_blank">Drupal.org</a> ?>
+            <a href="https://twitter.com/sampoturve" title="Twitter" class="twitter" target="_blank">Twitter</a>
+          </div>
+          <div class="duck-mate">
+            <span role="img" aria-label="Juice Box">🧃</span><span role="img" aria-label="Duck">🦆</span>
+          </div>
+        </div>
+      </article>
+    </main>
+  </div>
+  </div>
+</body>
+</html>
